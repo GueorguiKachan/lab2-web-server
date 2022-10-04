c66fded41d42dfa4c7e8daf3020123256275334d
GueorguiKachan <76787820+GueorguiKachan@users.noreply.github.com>
2022-10-04 21:40:11 +0200

    Update TimeComponent.kt

--- src/main/kotlin/es/unizar/webeng/lab2/TimeComponent.kt
+++ src/main/kotlin/es/unizar/webeng/lab2/TimeComponent.kt
@@ -1,9 +1,9 @@
 package es.unizar.webeng.lab2
 
+import java.time.LocalDateTime
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDateTime
 
 data class TimeDTO(val time: LocalDateTime)
 
@@ -18,7 +18,7 @@ class TimeService : TimeProvider {
 
 fun LocalDateTime.toDTO() = TimeDTO(time = this)
 
-@RestController  
+@RestController
 class TimeController(val service: TimeProvider) {
     @GetMapping("/time")
     fun time() = service.now().toDTO()
